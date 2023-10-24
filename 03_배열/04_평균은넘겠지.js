5aa36a2bc72372680b3068b288168528c1eb239c
Frontend <sweetysks23@gmail.com>
2023-10-24 23:45:41 +0900

    배열 문제풀이

---
+++ 03_배열/04_평균은넘겠지.js
@@ -0,0 +1,30 @@
+// 문제
+// 대학생 새내기들의 90%는 자신이 반에서 평균은 넘는다고 생각한다. 당신은 그들에게 슬픈 진실을 알려줘야 한다.
+
+// 입력
+// 첫째 줄에는 테스트 케이스의 개수 C가 주어진다.
+
+// 둘째 줄부터 각 테스트 케이스마다 학생의 수 N(1 ≤ N ≤ 1000, N은 정수)이 첫 수로 주어지고, 이어서 N명의 점수가 주어진다. 점수는 0보다 크거나 같고, 100보다 작거나 같은 정수이다.
+
+// 출력
+// 각 케이스마다 한 줄씩 평균을 넘는 학생들의 비율을 반올림하여 소수점 셋째 자리까지 출력한다. 정답과 출력값의 절대/상대 오차는 10-3이하이면 정답이다.
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+let testCase = Number(input[0]);
+for (let t = 1; t <= testCase; t++) {
+  // 각각의 테스트 케이스를 확인
+  let data = input[t].split(" ").map(Number);
+  let n = data[0]; // 첫번째 수는 데이터의 개수
+  let summary = 0;
+  for (let i = 1; i <= n; i++) {
+    summary += data[i];
+  }
+}
+
+let average = summary / n;
+let cnt = 0;
+for (let i = 1; i <= n; i++) {
+  if (data[i] > average) cnt += 1;
+}
+console.log(`${((cnt / n) * 100).toFixed(3)}%`);
