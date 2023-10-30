4dadd3e8b46230f70f765453bcacae8151c7edf7
Frontend <sweetysks23@gmail.com>
2023-10-30 21:54:10 +0900

    문자열 문제풀이

--- 04_문자열/05_단어의 개수.js
+++ 04_문자열/05_단어의 개수.js
@@ -16,7 +16,6 @@
 
 // • 단순히  전체 문자열에  포함된  단어의  개수를 계산한다.
 // • 결과적으로 공백(" ")으로 구분된 단어의 개수를 세면 된다.
-
 let fs = require("fs");
 let input = fs.readFileSync("/dev/stdin").toString().split("\n");
 // trim() 메서드는 문자열 양끝의공백을 제거한다.
