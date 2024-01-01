1290ca34f10d49b1e1b899f9612fb3a5446ac1d9
Frontend <sweetysks23@gmail.com>
2024-01-01 22:48:45 +0900

    문제풀이

--- ch1/03_배열/04_평균은넘겠지.js
+++ ch1/03_배열/04_평균은넘겠지.js
@@ -13,15 +13,19 @@ let fs = require("fs");
 let input = fs.readFileSync("/dev/stdin").toString().split("\n");
 
 let testCases = Number(input[0]);
+
 for (let t = 1; t <= testCases; t++) {
   let data = input[t].split(" ").map(Number);
   let n = data[0];
   let sum = 0;
+
   for (let i = 1; i <= n; i++) {
     sum += data[i];
   }
+
   let avg = sum / n;
   let cnt = 0;
+
   for (let i = 1; i <= n; i++) if (data[i] > avg) cnt += 1;
   console.log(`${((cnt / n) * 100).toFixed(3)}%`);
 }
